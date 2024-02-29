e948d03af24b4aec09b563fc406acd5df7a4a7eb
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-02-29 13:26:04 +0100

    Nested routes, outlet og layout tilføjet.

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,25 @@ import Login from "./security/Login";
 import Layout from "./Layout";
 import Home from "./Home";
 import "./App.css";
+import Contact from "./Contact";
+import RecipesLayout from "./recipes/RecipesLayout";
 
 export default function App() {
-  //const auth = useAuth();
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories/" element={<Categories />} />
-        <Route path="/recipes">
-          <Route index element={<Recipes />} />
-          <Route path=":id" element={<Recipe />} />
-        </Route>
-        <Route path="/add" element={<RecipeForm />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/logout" element={<Logout />} /> */}
-      </Routes>
-    </Layout>
-  );
+    //const auth = useAuth();
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/categories/" element={<Categories />} />
+                <Route path="/recipes" element={<RecipesLayout />}>
+                    <Route path="test" element={<h1>Test</h1>} />
+                    <Route path=":id" element={<Recipe />} />
+                </Route>
+                <Route path="/add" element={<RecipeForm />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<Login />} />
+                {/* <Route path="/logout" element={<Logout />} /> */}
+            </Routes>
+        </Layout>
+    );
 }
