5290d96b8fa7a0c336ae798e149021ab1838652f
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-03-01 15:56:06 +0100

    QueryParams and useSearchParams added

---
+++ src/Contact.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <>
+            <h2>Contact</h2>
+            <p>Write to us at: contact@mail.com</p>
+        </>)
+}
--- src/recipes/RecipesLayout.tsx
+++ src/recipes/RecipesLayout.tsx
@@ -2,6 +2,7 @@ import { useOutlet } from "react-router-dom";
 import RecipeList from "./RecipeList";
 import "./RecipesLayout.css";
 //import { useAuth } from "../security/_Authprovider";
+import { Outlet } from "react-router-dom";
 
 export default function RecipesLayout() {
   const outlet = useOutlet();
@@ -13,7 +14,7 @@ export default function RecipesLayout() {
       </div>
       <div className="outlet-container">
         {outlet || <h3>Select a recipe to see details</h3>}
-        {/* <Outlet /> */}
+        { <Outlet />}
       </div>
     </div>
   );
